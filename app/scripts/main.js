445ad28a5625b0f30c67a7532a699419dfdaea88
Toomean <arttumin@gmail.com>
2015-01-30 22:38:24 +0300

    Build version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -43,14 +43,41 @@ grapheme.AnimImages = function(settings) {
 grapheme.Contacts = function() {
 	var $parent = $('.block-contacts'),
 		$contBtn = $('.js-contacts'),
-		$closeBtn = $parent.find('.contacts-cross');
+		$closeBtn = $parent.find('.contacts-cross'),
+		$body = $('body');
 
 	$contBtn.click( function(){
-		$parent.addClass('active');
+		$body.addClass('contacts-active');
 	});
 
 	$closeBtn.click( function(){
-		$parent.removeClass('active');
+		$body.removeClass('contacts-active');
+	});
+}
+grapheme.Animsition = function(){
+	$(document).ready(function() {  
+	  	$(".animsition").animsition({	  
+	    	inClass               :   'fade-in',
+	    	outClass              :   'fade-out',
+	    	inDuration            :    1500,
+	    	outDuration           :    800,
+	    	linkElement           :   '.animsition-link',
+	    	// e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+	    	loading               :    true,
+	    	loadingParentElement  :   'body', //animsition wrapper element
+	    	loadingClass          :   'animsition-loading',
+	    	unSupportCss          : [ 'animation-duration',
+	        	                      '-webkit-animation-duration',
+	            	                  '-o-animation-duration'
+	                	            ],
+	    	//"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+	    	//The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+	    
+	    	overlay               :   false,
+	    
+	    	overlayClass          :   'animsition-overlay-slide',
+	    	overlayParentElement  :   'body'
+	  	});
 	});
 }
 
@@ -62,4 +89,5 @@ $(function(){
 	});
 	grapheme.AnimImages({});
 	grapheme.Contacts();
+	grapheme.Animsition();
 });
