a1590370f5426e3c0584f476d580505425def980
Владимир <vkharseev@gmail.com>
2015-03-19 14:20:41 +0300

    Контакты

--- app/views/templates/site/block-contacts.blade.php
+++ app/views/templates/site/block-contacts.blade.php
@@ -4,18 +4,11 @@
  */
 
 $contacts = Dictionary::valuesBySlug('contacts');
-#$contacts = DicLib::extracts($contacts, null, true, true);
-Helper::ta($contacts);
+$contacts = DicLib::extracts($contacts, null, true, true);
 ?>
 <div class="block-contacts">
     <div class="contacts-wrapper clearfix">
-
-        <div class="address">
-            <address>344006, Ростов-на-Дону, ул. Суворова 52а, офис 304</address>
-            <a class="map-link">Как добраться?</a>
-        </div>
-        <div class="phone"><a href="tel:+78632041941" class="tel-link">(863) 204-19-41</a><a href="tel:+79094281234" class="tel-link">+7 (909) 428-12-34</a></div>
-        <div class="mail"><a href="mailto:be.digital@grapheme.ru" class="mail-link">be.digital@grapheme.ru</a></div>
+        {{ isset($contacts[53]['description']) ? $contacts[53]['description'] : 'Контакты не установлены' }}
         <div class="contacts-cross">
             <div class="contacts-cross-inner">✕</div>
         </div>
